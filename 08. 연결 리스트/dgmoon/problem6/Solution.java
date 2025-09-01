35f2171091e4b4cc3eab584689e0ded3449dee9b
DonggeonMoon <nullsafer@naver.com>
2025-09-01 21:54:51 +0900

    feat: 8장 17번, 18번

---
+++ 08. 연결 리스트/dgmoon/problem6/Solution.java
@@ -0,0 +1,18 @@
+package problem6;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+
+class Solution {
+    public LinkedList oddEvenList(LinkedList head) {
+        //todo
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println("solution.oddEvenList(new LinkedList(List.of(1, 2, 3, 4, 5, 6)) = " + solution.oddEvenList(new LinkedList(List.of(1, 2, 3, 4, 5, 6))));
+    }
+}
